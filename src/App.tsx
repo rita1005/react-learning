67e1fffbceff87e60fd7a73804f67ac024f782e9
rita811005 <78014764+rita1005@users.noreply.github.com>
2023-12-11 03:17:10 +0000

    todoList

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,115 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <Homework />;
+  return (
+    <div>
+      <Sign />
+      <br />
+      <TodoList />
+    </div>
+  );
 }
 
-function Homework() {
+function Sign() {
   const [sign, setSign] = useState<string>();
-  const [name, setName] = useState<string>("");
+  const [name, setName] = useState<string>();
   const [signs, setSigns] = useState<string[]>([]);
   return (
-    <div className="App">
+    <>
+      <h3>Sign</h3>
+      <div className="App">
+        <input
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            setSign(name);
+            console.log(name);
+            setSigns([...signs, name!]);
+          }}
+        >
+          click
+        </button>
+        <div>{sign}您好</div>
+        <div>{name}</div>
+        <ul>
+          {signs.map((sign) => (
+            <li key={sign}>{sign}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function TodoList() {
+  const [todo, setTodo] = useState<string>("");
+  const [todoList, setTodoList] = useState<{ note: string; isDone: boolean }[]>(
+    [],
+  );
+  const handleButtonClick = () => {
+    setTodo("");
+  };
+
+  const handleCheckBoxChange = (index: number) => {
+    const updatedTodoList = [...todoList];
+    updatedTodoList[index].isDone = !updatedTodoList[index].isDone;
+    setTodoList(updatedTodoList);
+  };
+
+  const handleDeleteButton = (index: number) => {
+    const updateTodoList = todoList.filter((_, i) => i !== index);
+    setTodoList(updateTodoList);
+  };
+
+  return (
+    <>
+      <h3>Todo List</h3>
       <input
+        value={todo}
         onChange={(e) => {
-          setName(e.target.value);
+          setTodo(e.target.value.trim());
         }}
       />
       <button
         onClick={() => {
-          setSign(name);
-          console.log(name);
-          setSigns([...signs, name]);
+          if (todo!) {
+            setTodoList([...todoList, { note: todo!, isDone: false }]);
+          }
+          handleButtonClick();
         }}
       >
-        click
+        add
       </button>
-      <div>{sign}您好</div>
-      <div>{name}</div>
-      <ul>
-        {signs.map((sign) => (
-          <li key={sign}>{sign}</li>
+      <ul style={{ listStyle: "none" }}>
+        {todoList.map((t, index) => (
+          <li
+            key={index}
+            style={{
+              textAlign: "left",
+              textDecoration: t.isDone ? "line-through" : "none",
+            }}
+          >
+            <input
+              type="checkbox"
+              onChange={() => {
+                handleCheckBoxChange(index);
+              }}
+            />
+            {t.note}
+            <button
+              onClick={() => {
+                handleDeleteButton(index);
+              }}
+            >
+              delete
+            </button>
+          </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
 
